c9e0f3b7a675cc0a8a1aba2b542ac6bbc4eb8b2b
icorvus <business.wrona@gmail.com>
2022-11-14 22:30:13 +0100

    Cleaned up the code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const animalsList = ['🦁', '🐶', '🦄', '🐷', '🐻', '🐼', '🦉', '🐸', '🦁', '🐶', '🦄', '🐷', '🐻', '🐼', '🦉', '🐸']
+const animalsList = ['🦁', '🐶', '🦄', '🐷', '🐻', '🐼', '🦉', '🐸', '🦁', '🐶', '🦄', '🐷', '🐻', '🐼', '🦉', '🐸'];
 let animalsShown = [];
 
 const checkPair = () => {
@@ -11,40 +11,34 @@ const checkWin = () => {
     let win = true;
     tiles.forEach(tile => {
         if (!tile.classList.contains('disabled')) win = false;
-    })
+    });
     return win;
-}
+};
 
 const showWinText = () => {
     const info = document.querySelector('.info');
     info.textContent = "You won! Click reset to play again.";
-}
+};
 
 const tileClick = event => {
     if (animalsShown.length === 2){
-        animalsShown[0].classList.remove('disabled');
-        animalsShown[1].classList.remove('disabled');
-        if (checkPair()){
-            animalsShown[0].classList.add('disabled');
-            animalsShown[1].classList.add('disabled');
-        } else {
+        if (!checkPair()){
             animalsShown[0].classList.add('hidden');
             animalsShown[1].classList.add('hidden');
-        }
+            animalsShown[0].classList.remove('disabled');
+            animalsShown[1].classList.remove('disabled');
+       }
         animalsShown = [];
     } else {
         event.target.classList.remove('hidden');
-        event.target.classList.add('disabled')
+        event.target.classList.add('disabled');
         animalsShown.push(event.target);
         if (animalsShown.length === 2 && checkPair()){
-            animalsShown[0].classList.add('disabled');
-            animalsShown[1].classList.add('disabled');
             animalsShown = [];
         }
     }
-    console.log(checkWin())
     if (checkWin()) showWinText();
-}
+};
 
 const fillBoard = () => {
     const board = document.querySelector('.tile-container');
@@ -54,12 +48,12 @@ const fillBoard = () => {
         animal.addEventListener('click', tileClick);
         animal.classList.add('tile');
         animal.classList.add('hidden');
-        const animalIndex = Math.floor(Math.random() * animalsList.length)
+        const animalIndex = Math.floor(Math.random() * animalsList.length);
         animal.textContent = animalsList[animalIndex];
         board.appendChild(animal);
         animalsList.splice(animalIndex, 1);
     }
-}
+};
 
 const resetBtn = document.querySelector('.reset');
 resetBtn.addEventListener('click', () => window.location.reload());
