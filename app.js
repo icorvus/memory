6e1187e42146e3cbfa3d15de7d6b6accbc672284
icorvus <business.wrona@gmail.com>
2022-11-14 20:49:45 +0100

    Game is fully functioning now

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-const animals = {
-    lion: '🦁',
-    dog: '🐶',
-    unicorn: '🦄',
-    pig: '🐷',
-    bear: '🐻',
-    panda: '🐼',
-    owl: '🦉',
-    frog: '🐸',
-}
-
 const animalsList = ['🦁', '🐶', '🦄', '🐷', '🐻', '🐼', '🦉', '🐸', '🦁', '🐶', '🦄', '🐷', '🐻', '🐼', '🦉', '🐸']
 let animalsShown = [];
 
@@ -17,8 +6,24 @@ const checkPair = () => {
     return false;
 };
 
+const checkWin = () => {
+    const tiles = document.querySelectorAll('.tile');
+    let win = true;
+    tiles.forEach(tile => {
+        if (!tile.classList.contains('disabled')) win = false;
+    })
+    return win;
+}
+
+const showWinText = () => {
+    const info = document.querySelector('.info');
+    info.textContent = "You won! Click reset to play again.";
+}
+
 const tileClick = event => {
     if (animalsShown.length === 2){
+        animalsShown[0].classList.remove('disabled');
+        animalsShown[1].classList.remove('disabled');
         if (checkPair()){
             animalsShown[0].classList.add('disabled');
             animalsShown[1].classList.add('disabled');
@@ -29,17 +34,21 @@ const tileClick = event => {
         animalsShown = [];
     } else {
         event.target.classList.remove('hidden');
+        event.target.classList.add('disabled')
         animalsShown.push(event.target);
-        if (checkPair()){
+        if (animalsShown.length === 2 && checkPair()){
             animalsShown[0].classList.add('disabled');
             animalsShown[1].classList.add('disabled');
             animalsShown = [];
         }
     }
+    console.log(checkWin())
+    if (checkWin()) showWinText();
 }
 
 const fillBoard = () => {
     const board = document.querySelector('.tile-container');
+    board.textContent = '';
     for (let i = 0; i < 16; i++) {
         const animal = document.createElement('div');
         animal.addEventListener('click', tileClick);
@@ -51,5 +60,8 @@ const fillBoard = () => {
         animalsList.splice(animalIndex, 1);
     }
 }
+
+const resetBtn = document.querySelector('.reset');
+resetBtn.addEventListener('click', () => window.location.reload());
 
 fillBoard();
